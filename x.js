f54921ebc29e14b23c7d20e8c4372522eb7d530c
php0day <56468363+php0day@users.noreply.github.com>
2020-04-14 16:55:13 +0800

    Update x.js

--- x.js
+++ x.js
@@ -6,6 +6,14 @@ var _hmt = _hmt || [];
   s.parentNode.insertBefore(hm, s);
 })();
 
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?6a3dafd9bbc11fe2e021e27b8f3bdf39";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
  */
  setTimeout(function(){
   var arr=['http://3g.022gc.net/#xwy','http://3g.029gc.net/#xwy'];
