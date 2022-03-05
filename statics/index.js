50fb31528c8b9a4eaa49e1fabe5027e28127fca0
phpxapp <56468363+phpxapp@users.noreply.github.com>
2022-03-06 05:21:16 +0800

    Delete statics directory

--- statics/index.js
+++
@@ -1,3 +0,0 @@
-if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-    document.getElementById("incy").style.display="none";
-}
